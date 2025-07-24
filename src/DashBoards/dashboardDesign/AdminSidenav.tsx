09e5cf910a9fdfa02104a072c5d323c5ac8af33b
Gakenye8741 <brayogakenye04@gmail.com>
2025-07-24 17:23:05 +0300

    Updated Admin side Nav Icons

--- src/DashBoards/dashboardDesign/AdminSidenav.tsx
+++ src/DashBoards/dashboardDesign/AdminSidenav.tsx
@@ -7,6 +7,10 @@ import {
   LogOut,
   DollarSign,
   Ticket,
+  Camera,
+  Calendar,
+  FileText,
+  House,
 } from "lucide-react";
 import { useDispatch } from "react-redux";
 import { clearCredentials } from "../../features/Auth/AuthSlice";
@@ -15,12 +19,12 @@ const navItems = [
   { name: "Analytics", path: "analytics", icon: <TrendingUp className="text-indigo-400" /> },
   { name: "Manage Users", path: "AllUsers", icon: <Users className="text-blue-400" /> },
   { name: "Manage Bookings", path: "AllBookings", icon: <ClipboardList className="text-crimson-400" /> },
-  { name: "Manage Venues", path: "Allvenues", icon: <ClipboardList className="text-green-400" /> },
-  { name: "Manage Medias", path: "AllMedia", icon: <User className="text-red-400" /> },
-  { name: "Manage Events", path: "AllEvents", icon: <DollarSign className="text-yellow-400" /> },
-  { name: "Manage Paymnts", path: "AllPayments", icon: <User className="text-purple-400" /> },
+  { name: "Manage Venues", path: "Allvenues", icon: <House className="text-green-400" /> },
+  { name: "Manage Medias", path: "AllMedia", icon: <Camera className="text-yellow-400" /> }, // Media icon
+  { name: "Manage Events", path: "AllEvents", icon: <Calendar className="text-orange-400" /> }, // Event icon
+  { name: "Manage Payments", path: "AllPayments", icon: <DollarSign className="text-yellow-900" /> },
   { name: "Manage Support Tickets", path: "supportTickets", icon: <Ticket className="text-purple-400" /> },
-  { name: "Manage Ticket Types", path: "ticketTypes", icon: <Ticket className="text-purple-400" /> },
+  { name: "Manage Ticket Types", path: "ticketTypes", icon: <FileText className="text-teal-400" /> }, // File/Text icon
   { name: "My Profile", path: "adminprofile", icon: <User className="text-purple-400" /> },
   { name: "Logout", path: "#", icon: <LogOut className="text-red-500" /> },
 ];
