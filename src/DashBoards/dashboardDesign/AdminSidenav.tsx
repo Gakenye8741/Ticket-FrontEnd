d8a6770c1ffc66e17a528f2a67b23c3bbac7c4f1
Gakenye8741 <brayogakenye04@gmail.com>
2025-07-24 17:56:45 +0300

    added An Admin Navbar Text

--- src/DashBoards/dashboardDesign/AdminSidenav.tsx
+++ src/DashBoards/dashboardDesign/AdminSidenav.tsx
@@ -39,7 +39,11 @@ export const AdminSideNav = ({ onNavItemClick }: { onNavItemClick?: () => void }
   return (
     <aside className="h-full w-full p-4 bg-gray-900 text-white space-y-2 overflow-y-auto mt-15">
 
-      <h2 className="underline mt-5 flex align-middle justify-center">Admin Side NavBar</h2>
+     <h4 className="mt-1 flex items-center justify-center text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-yellow-500">
+      <span className="mr-5 ">🛠️</span> {/* Tool Emoji */}
+      Admin Side NavBar
+      <span className="ml-2">👑</span> {/* Crown Emoji */}
+    </h4>
       {navItems.map((item, index) =>
         item.name === "Logout" ? (
           <button
