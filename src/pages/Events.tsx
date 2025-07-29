6921b37003bfb3f4af456b1040fdfd05176c97c2
Gakenye8741 <brayogakenye04@gmail.com>
2025-07-29 11:51:04 +0300

    Refactored code

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,15 +1,20 @@
-import { Footer } from "../components/Footer"
-import { Navbar } from "../components/Navbar"
-import { EventDetailsPage } from "../content-folders/Events/EventsDetails"
+import { useEffect } from "react";
+import toast from "react-hot-toast";
 
-
+import { Footer } from "../components/Footer";
+import { Navbar } from "../components/Navbar";
+import { EventDetailsPage } from "../content-folders/Events/EventsDetails";
 
 export const Events = () => {
+  useEffect(() => {
+    toast.success("Welcome to the Events Page 🎟️");
+  }, []);
+
   return (
     <div>
-        <Navbar/>
-        <EventDetailsPage/>
-        <Footer/>
+      <Navbar />
+      <EventDetailsPage />
+      <Footer />
     </div>
-  )
-}
+  );
+};
