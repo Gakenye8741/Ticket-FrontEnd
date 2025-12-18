af0e2a18a82e695be4a95dd2d4e8e907c4ee76b7
Gakenye8741 <brayogakenye04@gmail.com>
2025-12-18 17:34:41 +0300

    Enable Native HTTP and update Android permissions

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.gekenye.ticket',
   appName: 'Ticketstream Events',
-  webDir: 'dist'
+  webDir: 'dist',
+  
+  // ADD THIS SECTION BELOW
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
 };
 
 export default config;
