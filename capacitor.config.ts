33bd93dd209a8ce181b3923241e048bfcd34ff89
Gakenye8741 <brayogakenye04@gmail.com>
2025-12-18 16:40:27 +0300

    Deploy ticketstream build workflow

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gekenye.ticket',
+  appName: 'Ticketstream Events',
+  webDir: 'dist'
+};
+
+export default config;
